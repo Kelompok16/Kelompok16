a96a911a127bcd4eefac85432dbff7e4a0a5cb84
Kelompok16 <kelompok167pmn1@gmail.com>
2022-01-02 22:41:39 +0700

    fixed new

--- phpmailer/index.php
+++ phpmailer/index.php
@@ -1,43 +1,50 @@
 <?php
-//Include required phpmailer files
-require 'includes/PHPMailer.php';
-require 'includes/SMTP.php';
-require 'includes/Exception.php';
-//Define name spaces
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP;
-use PHPMailer\PHPMailer\Exception;
-//Create instance of phpmailer
-$mail=new PHPMailer();
-//Set mailer to use smtp
-$mail->isSMTP();
-//define smtp host
-$mail->HOST = "smtp.gmail.com";
-//enable smtp authentication
-$mail->SMTPAuth = "true";
-//set type of encryption (ssl/tls)
-$mail->SMTPSecure = "tls";
-//set port to connect smtp
-$mail->Port = "587";
-//set gmail username
-$mail->Username = "kelompok167pmn1@gmail.com";
-//set gmail password
-$mail->Password = "rafimualif";
-//Set email subject
-$mail->Subject = "Using PHPMailer";
-//Set sender email
-$mail->setFrom("kelompok167pmn1@gmail.com");
-//Enable HTML
-$mail->isHTML(TRUE);
-//Email body
-$mail->body = "This is plain text email body";
-//Add recipient
-$mail->addAdress("kelompok167pmn1@gmail.com");
-//Finally send email
-if ($mail->Send ()) {
-    echo "Email Sent..!";
-}else{
-    echo "Error..!";
-}
-//closing smtp connecting
-$mail->smtpClose();
+ date_default_timezone_set('Etc/UTC');
+ require '../PHPMailerAutoload.php';
+ //Membuat instance PHPMailer baru
+ $mail = new PHPMailer;
+ //Memberi tahu PHPMailer untuk menggunakan SMTP
+ $mail->isSMTP();
+ //Mengaktifkan SMTP debugging
+ // 0 = off (digunakan untuk production)
+ // 1 = pesan client
+ // 2 = pesan client dan server
+ $mail->SMTPDebug = 2;
+ //HTML-friendly debug output
+ $mail->Debugoutput = 'html';
+ //hostname dari mail server
+ $mail->Host = 'smtp.gmail.com';
+ // gunakan
+ // $mail->Host = gethostbyname('smtp.gmail.com');
+ // jika jaringan Anda tidak mendukung SMTP melalui IPv6
+ //Atur SMTP port - 587 untuk dikonfirmasi TLS, a.k.a. RFC4409 SMTP submission
+ $mail->Port = 587;
+ //Set sistem enkripsi untuk menggunakan - ssl (deprecated) atau tls
+ $mail->SMTPSecure = 'tls';
+ //SMTP authentication
+ $mail->SMTPAuth = true;
+ //Username yang digunakan untuk SMTP authentication - gunakan email gmail
+ $mail->Username = "kelompok167pmn1@gmail.com";
+ //Password yang digunakan untuk SMTP authentication
+ $mail->Password = "rafimualif";
+ //Email pengirim
+ $mail->setFrom('kelompok167pmn1@gmail.com', 'First Last');
+ //Alamat email alternatif balasan
+ $mail->addReplyTo('kelompok167pmn1@gmail.com', 'First Last');
+ //Email tujuan
+ $mail->addAddress('kelompok167pmn1@gmail.com', 'John Doe');
+ //Subject email
+ $mail->Subject = 'PHPMailer GMail SMTP test';
+ //Membaca isi pesan HTML dari file eksternal, mengkonversi gambar yang di embed,
+ //Mengubah HTML menjadi basic plain-text
+ $mail->msgHTML(file_get_contents('contents.html'), dirname(__FILE__));
+ //Replace plain text body dengan cara manual
+ $mail->AltBody = 'This is a plain-text message body';
+ //Attach file gambar
+ $mail->addAttachment('images/phpmailer_mini.png');
+ //mengirim pesan, mengecek error
+ if (!$mail->send()) {
+ echo "Email Error: " . $mail->ErrorInfo;
+ } else {
+ echo "Pesan Terkirim!";
+ }
